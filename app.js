8b5848c69e1335e2ddc67dcb9b25857495031fdf
Rajesh Ligal <rajeshligal21@gmail.com>
2020-08-23 01:08:16 +0545

    Adds Chain Route

--- app.js
+++ app.js
@@ -28,43 +28,45 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
-  Article.find(function (err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
 
-app.post("/articles", function (req, res) {
-  console.log(req.body.title);
-  console.log(req.body.content);
+//there no semicolon in chain, because you dont want them to end
+app
+  .route("/articles")
 
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
 
-  newArticle.save(function (err) {
-    if (!err) {
-      res.send("Successfully added a new article.");
-    } else {
-      res.send(err);
-    }
-  });
-});
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
 
-app.delete("/articles", function (req, res) {
-  Article.deleteMany(function (err) {
-    if (!err) {
-      res.send("Deleted all articles successfully");
-    } else {
-      res.send(err);
-    }
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Deleted all articles successfully");
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
